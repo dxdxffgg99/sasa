e45fde68221f9aee54a58c94299b4f03773d7761
dxdxffgg99 <dxdxffgg99@gmail.com>
2026-08-01 10:03:24 +0900

    game init

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ import cv2
 import mediapipe as mp
 import pygame
 
+pygame.init()
+
+HEIGHT = pygame.display.Info().current_h
+WIDTH = 768
+BG_COLOR = (0,0,0)
+GAME_CAPTION = "sasa rhythm"
+
 try:
     with open('games.json', 'r', encoding='utf-8') as file:
         content = file.read()
@@ -19,3 +26,19 @@ except json.JSONDecodeError:
 except Exception as e:
     print(f"{e}")
     exit(0)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT-50))
+pygame.display.set_caption(GAME_CAPTION)
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(BG_COLOR) 
+
+    pygame.display.flip()
+
+pygame.quit()
