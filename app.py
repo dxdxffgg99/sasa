8c11eaeb2091add84813294c7e1060ba5b7493a0
dxdxffgg99 <dxdxffgg99@gmail.com>
2026-08-01 09:52:59 +0900

    fix n init

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import random
 import time
+import json
+import os
 
 import cv2
 import mediapipe as mp
 import pygame
-from Box2D import b2World
+
+try:
+    with open('games.json', 'r', encoding='utf-8') as file:
+        content = file.read()
+        json.loads(content)
+
+except json.JSONDecodeError:
+    print(f"err: JSONDecodeError")
+    exit(0)
+    
+except Exception as e:
+    print(f"{e}")
+    exit(0)
